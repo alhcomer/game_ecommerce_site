f9bd9fa7325218d60988a02c8bf82d00c615511e
Aidan Comer <alhcomer@hotmail.co.uk>
2022-09-23 13:38:48 +0100

    implemented initial admin models

--- game_ecommerce_site/settings.py
+++ game_ecommerce_site/settings.py
@@ -15,7 +15,7 @@ import environ
 import os
 
 env = environ.Env()
-environ.Env.read_env()
+env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
--- gaming_shop/admin.py
+++ gaming_shop/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Category, Product, Platform
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)}
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title', 'description', 'slug', 'price', 
+                    'developer', 'publisher', 'created',
+                    'updated', 'in_stock', 'is_active']
+    list_filter = ['in_stock', 'is_active']
+    list_editable = ['price', 'in_stock', 'is_active']
+    prepopulated_fields = {'slug': ('title',)}
+
+@admin.register(Platform)
+class PlatformAdmin(admin.ModelAdmin):
+    list_display = ['name',]
