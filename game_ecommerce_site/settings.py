5eb0131fe57b5f09e9f120cf81757e19a1ee3296
Aidan Comer <alhcomer@hotmail.co.uk>
2022-10-26 10:36:18 +0100

    began implementing basket/sessions

--- game_ecommerce_site/settings.py
+++ game_ecommerce_site/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["gamestation.com"]
+ALLOWED_HOSTS = ["gamestation.com", "127.0.0.1"]
 
 
 # Application definition
@@ -127,6 +127,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static")
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
