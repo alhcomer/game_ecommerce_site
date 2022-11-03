e811adb233d84567e8866053c7e6592a2eff23cf
Aidan Comer <alhcomer@hotmail.co.uk>
2022-11-03 21:28:33 +0000

    added basket session product add capability

--- basket/basket.py
+++ basket/basket.py
@@ -7,5 +7,13 @@ class Basket:
         self.session = request.session
         basket = self.session.get('session_key')
         if 'session_key' not in request.session:
-            basket = self.session['session_key'] = {'number': 123123}
+            basket = self.session['session_key'] = {}
         self.basket = basket
+
+    def add(self, product):
+        product_id = product.id
+        if product_id not in self.basket:
+            self.basket[product_id] = {'price': product.price}
+
+        self.session.modified = True
+
--- basket/views.py
+++ basket/views.py
@@ -1,4 +1,17 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.shortcuts import get_object_or_404, redirect, render
+from .basket import Basket
+from gaming_shop.models import Product
 
 def basket_summary(request):
     return render(request, 'basket/summary.html')
+
+def basket_add(request):
+    basket = Basket(request)
+    if request.POST.get('action') == 'POST':
+        product_id = int(request.POST.get('productid'))
+        product = get_object_or_404(Product, id=product_id)
+        basket.add(product=product)
+
+        response = JsonResponse({'test': 'data'})
+        return response
