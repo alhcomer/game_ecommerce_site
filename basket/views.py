d645f5c637f73de4f59b21cad5e9e58b20737efc
Aidan Comer <alhcomer@hotmail.co.uk>
2022-11-04 17:22:36 +0000

    added TODOs

--- basket/views.py
+++ basket/views.py
@@ -2,6 +2,17 @@ from django.http import JsonResponse
 from django.shortcuts import get_object_or_404, redirect, render
 from .basket import Basket
 from gaming_shop.models import Product
+from django.core import serializers
+from decimal import Decimal
+import json
+from django.core.serializers.json import DjangoJSONEncoder
+from django.core.serializers import serialize
+
+# class LazyEncoder(DjangoJSONEncoder):
+#     def default(self, obj):
+#         if isinstance(obj, YourCustomType):
+#             return str(obj)
+#         return super().default(obj)
 
 def basket_summary(request):
     return render(request, 'basket/summary.html')
@@ -11,7 +22,9 @@ def basket_add(request):
     if request.POST.get('action') == 'POST':
         product_id = int(request.POST.get('productid'))
         product = get_object_or_404(Product, id=product_id)
+        # TODO: must make product object json serializable
+        # print(product)
+        # product = serialize('json', product, cls=LazyEncoder)
         basket.add(product=product)
-
         response = JsonResponse({'test': 'data'})
         return response
