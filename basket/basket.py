b516e8d96ad5fa9108130ba2316247485fbb7a1a
Aidan Comer <alhcomer@hotmail.co.uk>
2022-11-08 19:16:15 +0000

    began rebuilding summary page

--- basket/basket.py
+++ basket/basket.py
@@ -9,11 +9,13 @@ class Basket:
         if 'session_key' not in request.session:
             basket = self.session['session_key'] = {}
         self.basket = basket
+        print(basket)
+        
 
     def add(self, product, quantity):
         product_id = product.id
         if product_id not in self.basket:
-            self.basket[str(product_id)] = {'price': str(product.price), 'quantity': int(quantity)}
+            self.basket[str(product_id)] = {'price': str(product.price), 'quantity': int(quantity), 'image_url': str(product.image.url)}
 
         self.session.modified = True
     
