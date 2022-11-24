17889ec01b9eef7bbbce6308153b3dcce5ee28a6
Aidan Comer <alhcomer@hotmail.co.uk>
2022-11-24 11:44:30 +0000

    created initial login page

--- gaming_shop/urls.py
+++ gaming_shop/urls.py
@@ -9,5 +9,6 @@ app_name = 'shop'
 urlpatterns = [
     path('', views.index, name='index'),
     path('games/<slug:slug>', views.product_item, name='product_item'),
-    path('shop/<slug:category_slug>/', views.category_list, name='category_list')
+    path('shop/<slug:category_slug>/', views.category_list, name='category_list'),
+    path('login/', views.login, name='login')
 ]
--- gaming_shop/views.py
+++ gaming_shop/views.py
@@ -16,3 +16,6 @@ def category_list(request, category_slug):
     category = get_object_or_404(Category, slug=category_slug)
     products = Product.objects.filter(category=category)
     return render(request, 'gaming_shop/products/category.html', {'category': category, 'products': products})
+
+def login(request):
+    return render(request, 'gaming_shop/login.html')
